a6541b9ea657881e61018da94d829b8d14ce9f79
amkera <60901491+amkera@users.noreply.github.com>
2020-11-15 11:21:49 -0500

    creating CRUD in both backend controllers

--- app/controllers/api/v1/payments_controller.rb
+++ app/controllers/api/v1/payments_controller.rb
@@ -1,2 +1,37 @@
 class Api::V1::PaymentsController < ApplicationController
+
+  def index
+    @payments = Payment.all
+    render json: @payments
+  end
+
+  def show
+    find_payment
+    render json: @payment
+  end
+
+  def create
+    @payment = Payment.new(payment_params)
+    if @payment.save
+      render json: @payment
+    else
+      render json: {error: "Oops! There was an error creating this payment."}
+  end
+
+  def destroy
+    find_payment
+    @payment.destroy
+  end
+
+  private
+
+  def payment_params
+    params.require(:payment).permit(:student_id, :date, :amount, :paid)
+  end
+
+  def find_payment
+    @payment = Payment.find(params[:id])
+  end
+
+
 end
--- app/controllers/api/v1/students_controller.rb
+++ app/controllers/api/v1/students_controller.rb
@@ -1,2 +1,46 @@
 class Api::V1::StudentsController < ApplicationController
+
+  def index
+    @students = Student.all
+    render json: @students
+  end
+
+
+  def show
+    find_student
+    render json: @student
+
+  end
+  #api/v1/student/1
+
+
+  def create
+    @student = Student.new(account_params)
+    if @student.save
+      render json: @student
+    else
+      render json: {error: "Oops! There was an error creating this student."}
+  end
+
+
+  def destroy
+    find_student
+    @student.destroy
+  end
+
+
+
+
+
+  private
+
+  def student_params
+    params.require(:student).permit(:name, :bio, :allergies)
+  end
+
+  def find_student
+    @student = Student.find(params[:id])
+  end
+
+
 end
